10aef6ed74ad1d5f236c23bb91e1c30b09b775cc
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-03-05 13:20:05 -0800

    push attempt 3?

--- adventure/admin.py
+++ adventure/admin.py
@@ -1,8 +1,8 @@
 # admin allows you to auto build a site to crud records, useful during testing?
 from django.contrib import admin
 from .models import Chamber, Player
-# from .mars import Mars
+from .mars import Mars
 
-# admin.site.register(Mars)
+admin.site.register(Mars)
 admin.site.register(Chamber)
 admin.site.register(Player)
